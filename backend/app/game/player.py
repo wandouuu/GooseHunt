2e1b4a011d1b8f8a7fe315fae45aa306927b18d3
wandouuu <130308899+wandouuu@users.noreply.github.com>
2026-03-07 17:04:21 -0500

    Refactor Player class variables

--- backend/app/game/player.py
+++ backend/app/game/player.py
@@ -1,8 +1,8 @@
 class Player:
     # role = 0 for hider and 1 for seeker
-    def __init__(self, id, name, lat, lon):
-        self.id = id
-        self.name = name
+    def __init__(self, player_id, player_name, lat, lon):
+        self.player_id = player_id
+        self.name = player_name
         self.lat = lat
         self.lon = lon
         self.role = None
