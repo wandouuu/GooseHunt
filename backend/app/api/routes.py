d86dbb7f81d639391a34ae3eace77596d507e697
wandouuu <130308899+wandouuu@users.noreply.github.com>
2026-03-07 14:25:01 -0500

    Add create_game route

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,1 +1,19 @@
-from FastAPI import FastAPI
+from FastAPI import APIRouter, HTTPException
+import app.game.game_manager as game_manager
+router = APIRouter()
+
+@router.post("/create_game")
+async def create_game():
+    game_id = game_manager.create_game()
+
+    if game_id is None:
+        raise HTTPException(status_code = 400, detail="Cannot create more than one game")
+    return {"game_id": game_id}
+
+
+@router.post("/join_game/{game_id}")
+async def join_game(game_id: str):
+    pass
+
+
+
